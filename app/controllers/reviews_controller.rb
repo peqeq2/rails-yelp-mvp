10ecbcaad9d1640a3148a0ba1419aeeb99b855ea
sergio santana <sergiosantana.m@gmail.com>
2023-08-16 19:22:40 -0400

    restaurant challeng not finish

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,6 @@
 class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.fin(params[:restaurant_id])
+    @review = Review.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :restaurants, only: %i[index create show]
+  resources :restaurants, only: %i[index create show new]
+  resources :reviews, only: [:new]
 end
